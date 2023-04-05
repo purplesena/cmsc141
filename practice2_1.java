b70d64b736a9cc89e55286b9af1b48189388bc0d
purplesena <54860049+purplesena@users.noreply.github.com>
2023-04-05 07:20:41 -0700

    Add files via upload

---
+++ practice2_1.java
@@ -0,0 +1,17 @@
+package package2_1;
+
+public class practice2_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("  %%%%  ");
+		System.out.println(" %    % ");
+		System.out.println("% %  % %");
+		System.out.println("%      %");
+		System.out.println("% %  % %");
+		System.out.println("%  %%  %");
+		System.out.println(" %    % ");
+		System.out.println("  %%%%  ");
+	}
+
+}
